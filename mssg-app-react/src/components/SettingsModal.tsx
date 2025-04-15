cd5badefcab07052cd00665e8fd6d74651a6dd62
Alex Ouyang <aouy006@aucklanduni.ac.nz>
2025-04-16 02:32:10 +1200

    completed logout functionality.

--- mssg-app-react/src/components/SettingsModal.tsx
+++ mssg-app-react/src/components/SettingsModal.tsx
@@ -1,17 +1,31 @@
 import { settingsModalProps } from "./types/settingsModal"
 import "./SettingsModal.css";
+import { useNavigate } from "react-router-dom";
+import { sendPostRequest } from "./utils";
 
 const SettingsModal = ({settingsModalRef} : settingsModalProps) => {
+  const navigate = useNavigate();
+
   const closeModal = (event: React.MouseEvent<HTMLDialogElement>) => {
     if (event.target === settingsModalRef.current) {
       settingsModalRef.current?.close();
     }
   };
+
+  const handleLogout = async (event: React.MouseEvent<HTMLLIElement>) => {
+    event.preventDefault();
+    console.log('hi');
+    const logoutEndpoint = `http://localhost:8000/user/logout/`;
+    const response = await sendPostRequest(logoutEndpoint, {})
+    console.log(response);
+    navigate("/login");
+  }
+
   return (
     <dialog ref={settingsModalRef} onClick={closeModal} className="settings-modal">
       <div className="inner-settings-modal">
         <ul className="setting-options">
-          <li className="logout-li">
+          <li onClick={handleLogout} className="logout-li">
             Logout
           </li>
         </ul>
