cc3564c6fe803c9e19ebe1e86a0acfd70a0a79c6
Alex Ouyang <aouy006@aucklanduni.ac.nz>
2025-05-18 02:05:13 +1200

    Added loading animations to register and login pages.

--- mssg-app-react/src/components/Login.tsx
+++ mssg-app-react/src/components/Login.tsx
@@ -7,6 +7,8 @@ const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [showAlert, setShowAlert] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [loginSuccessful, setLoginSuccessful] = useState<boolean>(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -32,6 +34,8 @@ const Login = () => {
       const csrfCookie = getCookie('csrftoken');
       if (!csrfCookie) throw new Error('CSRF cookie could not be obtained from the browser!');
 
+      setIsLoading(true);
+
       const loginUrl = 'http://localhost:8000/user/login/';
       const userData = { username: username, password: password };
 
@@ -52,12 +56,19 @@ const Login = () => {
         throw new Error(errorMessage);
       }
 
+      setIsLoading(false);
+      setLoginSuccessful(true);
+
       const transformedData = convertSnakeToCamel(data);
 
       const loggedInUser: User = transformedData.user;
       console.log('Successfully logged in.');
       localStorage.setItem('user', JSON.stringify(loggedInUser));
-      navigate('/message');
+
+      setTimeout(() => {
+        navigate('/message');
+      }, 1000);
+
     } catch (error: any) {
       console.error(error);
       setShowAlert(true);
@@ -67,7 +78,7 @@ const Login = () => {
   const closeAlert = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     setShowAlert(false);
-  }
+  };
 
   const toRegister = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
@@ -83,69 +94,124 @@ const Login = () => {
   };
 
   return (
-    <div className="flex relative left-1/2 top-1/2 rounded-md -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2 border-1">
-      <div className="flex flex-col text-white justify-center items-center w-1/2 h-full rounded-r-full bg-gray-400">
-        <h1 className="text-center font-semibold text-4xl p-2">Hello, Welcome!</h1>
-        <p className="text-center font-light p-4 mb-2">Don't have an account? </p>
-        <button
-          onClick={toRegister}
-          className="py-1 rounded-md cursor-pointer hover:scale-102 font-medium px-10 border-1"
-        >
-          Register
-        </button>
-      </div>
-      <div className="flex flex-col justify-center items-center w-1/2 py-15 px-2">
-        <h1 className="text-2xl font-semibold mb-2">Login</h1>
+    <>
+      {loginSuccessful && (
         <div
-          className={`${
-            showAlert ? 'visible' : 'invisible'
-          } bg-red-100 text-sm border relative border-red-400 w-3/4 text-red-700 px-1 py-3 rounded
-          `}
+          className="bg-teal-100 fixed left-1/2 -translate-x-1/2 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
           role="alert"
         >
-          <span className="block sm:inline">Incorrect username or password.</span>
-          <span className="absolute top-0 right-0 pr-2 py-3">
-            <button onClick={closeAlert}>
+          <div className="flex">
+            <div className="py-1">
               <svg
-                className="fill-current h-5 w-5 cursor-pointer hover:scale-120 text-red-500"
-                role="button"
+                className="fill-current h-6 w-6 text-teal-500 mr-4"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
               >
-                <title>Close</title>
-                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
               </svg>
-            </button>
-          </span>
+            </div>
+            <div>
+              <p className="font-bold">Login Sucessful!</p>
+              <p className="text-sm">Enjoy your time.</p>
+            </div>
+          </div>
         </div>
-        <form
-          className="flex items-center justify-center flex-col w-full h-full p-[3%]"
-          onSubmit={handleSubmit}
-        >
-          <input
-            className="text-black mt-[15px] text-sm w-3/4 rounded-lg bg-[#f5f5f5] mb-[10px] p-2 outline-none"
-            type="text"
-            id="login-username"
-            value={username}
-            onChange={updateUsername}
-            placeholder="Username"
-          />
-          <input
-            className="text-black mt-[15px] w-3/4 text-sm mb-[10px] rounded-lg p-2 bg-[#f5f5f5] outline-none"
-            type="password"
-            id="login-password"
-            value={password}
-            onChange={updatePassword}
-            placeholder="Password"
-          />
-          <input
-            className="text-white text-md font-[450] mt-[20px] p-1.5 w-3/4 mb-[10px] bg-black cursor-pointer rounded-lg"
-            type="submit"
-            value="Login"
-          />
-        </form>
+      )}
+      <div className="flex relative left-1/2 top-1/2 rounded-md -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2 border-1">
+        <div className="flex flex-col text-white justify-center items-center w-1/2 h-full rounded-r-full bg-gray-400">
+          <h1 className="text-center font-semibold text-4xl p-2">Hello, Welcome!</h1>
+          <p className="text-center font-light p-4 mb-2">Don't have an account? </p>
+          <button
+            onClick={toRegister}
+            className="py-1 rounded-md cursor-pointer hover:scale-102 font-medium px-10 border-1"
+          >
+            Register
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center w-1/2 py-15 px-2">
+          <h1 className="text-2xl font-semibold mb-2">Login</h1>
+          <div
+            className={`${
+              showAlert ? 'visible' : 'invisible'
+            } bg-red-100 text-sm border relative border-red-400 w-3/4 text-red-700 px-1 py-3 rounded
+          `}
+            role="alert"
+          >
+            <span className="block sm:inline">Incorrect username or password.</span>
+            <span className="absolute top-0 right-0 pr-2 py-3">
+              <button onClick={closeAlert}>
+                <svg
+                  className="fill-current h-5 w-5 cursor-pointer hover:scale-120 text-red-500"
+                  role="button"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <title>Close</title>
+                  <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                </svg>
+              </button>
+            </span>
+          </div>
+          <form
+            className="flex items-center justify-center flex-col w-full h-full p-[3%]"
+            onSubmit={handleSubmit}
+          >
+            <input
+              className="text-black mt-[15px] text-sm w-3/4 rounded-lg bg-[#f5f5f5] mb-[10px] p-2 outline-none"
+              type="text"
+              id="login-username"
+              value={username}
+              onChange={updateUsername}
+              placeholder="Username"
+            />
+            <input
+              className="text-black mt-[15px] w-3/4 text-sm mb-[10px] rounded-lg p-2 bg-[#f5f5f5] outline-none"
+              type="password"
+              id="login-password"
+              value={password}
+              onChange={updatePassword}
+              placeholder="Password"
+            />
+            <button
+              type="submit"
+              className="hover:scale-101 flex items-center justify-center text-white text-md font-[450] mt-[20px] p-2.5 h-10 w-3/4 mb-[10px] bg-black cursor-pointer rounded-lg"
+            >
+              {isLoading ? (
+                <>
+                  <svg className="left-0 animate-spin h-4 w-4" viewBox="0 0 24 24">
+                    <circle
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                      fill="none"
+                      strokeDasharray="32"
+                      strokeDashoffset="32"
+                    >
+                      <animate
+                        attributeName="stroke-dasharray"
+                        dur="2s"
+                        values="0 32;16 16;0 32;0 32"
+                        repeatCount="indefinite"
+                      />
+                      <animate
+                        attributeName="stroke-dashoffset"
+                        dur="2s"
+                        values="0;-16;-32;-32"
+                        repeatCount="indefinite"
+                      />
+                    </circle>
+                  </svg>
+                </>
+              ) : (
+                <>Login</>
+              )}
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- mssg-app-react/src/components/Register.tsx
+++ mssg-app-react/src/components/Register.tsx
@@ -5,8 +5,10 @@ import { getCookie } from './utils';
 const Register = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState<string>("Invalid Input");
-  const [showAlert, setShowAlert] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>('Invalid Input');
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+  const [registrationSucessful, setRegistrationSucessful] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -41,11 +43,12 @@ const Register = () => {
 
   const toLogin = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    navigate("/login");
-  }
+    navigate('/login');
+  };
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setIsLoading(true);
     const registerUrl = 'http://localhost:8000/user/register/';
     try {
       const userData = { username: username, password: password };
@@ -68,78 +71,138 @@ const Register = () => {
         throw new Error(data.error);
       }
 
-      navigate('/login');
+      setIsLoading(false);
+      setRegistrationSucessful(true);
+      setTimeout(() => {
+        navigate('/login');
+      }, 1500);
+
     } catch (error: any) {
-      if (error.message === "Username already exists") {
+      if (error.message === 'Username already exists') {
         setErrorMessage(error.message);
       }
       setShowAlert(true);
     }
   };
   return (
-    <div className="flex relative left-1/2 top-1/2 rounded-md -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2 border-1">
-      <div className="flex flex-col text-white justify-center items-center w-1/2 h-full rounded-r-full bg-gray-400">
-        <h1 className="text-center font-semibold text-4xl p-2">Hello, Welcome!</h1>
-        <p className="text-center font-light p-4 mb-2">Already have an account? </p>
-        <button
-          onClick={toLogin}
-          className="py-1 rounded-md cursor-pointer hover:scale-102 font-medium px-10 border-1"
-        >
-          Login
-        </button>
-      </div>
-      <div className="flex flex-col justify-center items-center w-1/2 py-15 px-2">
-        <h1 className="text-2xl font-semibold mb-2">Register</h1>
+    <>
+      {registrationSucessful && (
         <div
-          className={`${
-            showAlert ? 'visible' : 'invisible'
-          } bg-red-100 text-sm border relative border-red-400 w-3/4 h-10 text-red-700 px-1 py-2 rounded
-          `}
+          className="bg-teal-100 fixed left-1/2 -translate-x-1/2 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
           role="alert"
         >
-          <span className="block sm:inline">{errorMessage}</span>
-          <span className="absolute top-0 right-0 pr-2 py-2">
-            <button onClick={closeAlert}>
+          <div className="flex">
+            <div className="py-1">
               <svg
-                className="fill-current h-5 w-5 cursor-pointer hover:scale-120 text-red-500"
-                role="button"
+                className="fill-current h-6 w-6 text-teal-500 mr-4"
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
               >
-                <title>Close</title>
-                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
               </svg>
-            </button>
-          </span>
+            </div>
+            <div>
+              <p className="font-bold">Registration Sucessful!</p>
+              <p className="text-sm">Please login with your account.</p>
+            </div>
+          </div>
         </div>
-        <form
-          className="flex items-center justify-center flex-col w-full h-full p-[3%]"
-          onSubmit={handleSubmit}
-        >
-          <input
-            className="text-black mt-[15px] text-sm w-3/4 rounded-lg bg-[#f5f5f5] mb-[10px] p-2 outline-none"
-            type="text"
-            id="login-username"
-            value={username}
-            onChange={updateUsername}
-            placeholder="Username"
-          />
-          <input
-            className="text-black mt-[15px] w-3/4 text-sm mb-[10px] rounded-lg p-2 bg-[#f5f5f5] outline-none"
-            type="password"
-            id="login-password"
-            value={password}
-            onChange={updatePassword}
-            placeholder="Password"
-          />
-          <input
-            className="text-white text-md font-[450] mt-[20px] p-1.5 w-3/4 mb-[10px] bg-black cursor-pointer rounded-lg"
-            type="submit"
-            value="Register"
-          />
-        </form>
+      )}
+      <div className="flex relative left-1/2 top-1/2 rounded-md -translate-x-1/2 -translate-y-1/2 w-1/2 h-1/2 border-1">
+        <div className="flex flex-col text-white justify-center items-center w-1/2 h-full rounded-r-full bg-gray-400">
+          <h1 className="text-center font-semibold text-4xl p-2">Hello, Welcome!</h1>
+          <p className="text-center font-light p-4 mb-2">Already have an account? </p>
+          <button
+            onClick={toLogin}
+            className="py-1 rounded-md cursor-pointer hover:scale-102 font-medium px-10 border-1"
+          >
+            Login
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center w-1/2 py-15 px-2">
+          <h1 className="text-2xl font-semibold mb-2">Register</h1>
+          <div
+            className={`${
+              showAlert ? 'visible' : 'invisible'
+            } bg-red-100 text-sm border relative border-red-400 w-3/4 h-10 text-red-700 px-1 py-2 rounded
+          `}
+            role="alert"
+          >
+            <span className="block sm:inline">{errorMessage}</span>
+            <span className="absolute top-0 right-0 pr-2 py-2">
+              <button onClick={closeAlert}>
+                <svg
+                  className="fill-current h-5 w-5 cursor-pointer hover:scale-120 text-red-500"
+                  role="button"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <title>Close</title>
+                  <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                </svg>
+              </button>
+            </span>
+          </div>
+          <form
+            className="flex items-center justify-center flex-col w-full h-full p-[3%]"
+            onSubmit={handleSubmit}
+          >
+            <input
+              className="text-black mt-[15px] text-sm w-3/4 rounded-lg bg-[#f5f5f5] mb-[10px] p-2 outline-none"
+              type="text"
+              id="login-username"
+              value={username}
+              onChange={updateUsername}
+              placeholder="Username"
+            />
+            <input
+              className="text-black mt-[15px] w-3/4 text-sm mb-[10px] rounded-lg p-2 bg-[#f5f5f5] outline-none"
+              type="password"
+              id="login-password"
+              value={password}
+              onChange={updatePassword}
+              placeholder="Password"
+            />
+            <button
+              type="submit"
+              className="hover:scale-101 flex items-center justify-center text-white text-md font-[450] mt-[20px] p-2.5 h-10 w-3/4 mb-[10px] bg-black cursor-pointer rounded-lg"
+            >
+              {isLoading ? (
+                <>
+                  <svg className="left-0 animate-spin h-4 w-4" viewBox="0 0 24 24">
+                    <circle
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                      fill="none"
+                      strokeDasharray="32"
+                      strokeDashoffset="32"
+                    >
+                      <animate
+                        attributeName="stroke-dasharray"
+                        dur="2s"
+                        values="0 32;16 16;0 32;0 32"
+                        repeatCount="indefinite"
+                      />
+                      <animate
+                        attributeName="stroke-dashoffset"
+                        dur="2s"
+                        values="0;-16;-32;-32"
+                        repeatCount="indefinite"
+                      />
+                    </circle>
+                  </svg>
+                </>
+              ) : (
+                <>Register</>
+              )}
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
